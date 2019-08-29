3d138b2c8f70e829bd4a22d22778521042eb61f2
ユーザー名 <kanokuda.1@gmail.com>
2019-08-29 18:43:57 +0900

    発展課題category導入途中

--- app/src/main/java/jp/techacademy/okuda/kanji/taskapp/Task.kt
+++ app/src/main/java/jp/techacademy/okuda/kanji/taskapp/Task.kt
@@ -1,15 +1,16 @@
 package jp.techacademy.okuda.kanji.taskapp
 
+import android.renderscript.Sampler
 import java.io.Serializable
-import java.util.Date
 import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
+import java.util.*
 
 open class Task : RealmObject(), Serializable {
     var title: String = ""      // タイトル
     var contents: String = ""   // 内容
     var date: Date = Date()     // 日時
-
+    var category :Locale.Category = Locale.Category.valueOf("")
     // id をプライマリーキーとして設定
     @PrimaryKey
     var id: Int = 0
