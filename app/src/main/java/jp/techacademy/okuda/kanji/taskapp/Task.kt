a8ca6291576621cfaa86e3be39a98775219894e6
ユーザー名 <kanokuda.1@gmail.com>
2019-08-31 22:05:29 +0900

    8/31 category実装中

--- app/src/main/java/jp/techacademy/okuda/kanji/taskapp/Task.kt
+++ app/src/main/java/jp/techacademy/okuda/kanji/taskapp/Task.kt
@@ -10,7 +10,7 @@ open class Task : RealmObject(), Serializable {
     var title: String = ""      // タイトル
     var contents: String = ""   // 内容
     var date: Date = Date()     // 日時
-    var mCategory:Category? = null   //
+    var mCategory:Category? =  Category()
 
     // id をプライマリーキーとして設定
     @PrimaryKey
