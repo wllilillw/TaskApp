011f12d64724ef2c4f9666f2db15d8b9fb67dc43
ユーザー名 <kanokuda.1@gmail.com>
2019-09-06 18:20:13 +0900

    20190906

--- app/src/main/java/jp/techacademy/okuda/kanji/taskapp/categorySet.kt
+++ app/src/main/java/jp/techacademy/okuda/kanji/taskapp/categorySet.kt
@@ -16,21 +16,13 @@ class categorySet : AppCompatActivity() {
     private val mOnCategoryClickListener = View.OnClickListener {
         addCategory2()
     }
-    var categoryList: MutableList<String> = arrayListOf()
+    //var categoryList: MutableList<String> = arrayListOf()
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category_set)
-
-
-
-/*        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
-        setSupportActionBar(toolbar)
-        if (supportActionBar != null) {
-            supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-        }*/
 
         buttonTouroku.setOnClickListener(mOnCategoryClickListener)
 
@@ -46,11 +38,11 @@ class categorySet : AppCompatActivity() {
             // 新規作成の場合
             mCategory = Category()
 
-            val categoryRealmResults = realm.where(Category::class.java).findAll()
+            val categoryRealmResults2 = realm.where(Category::class.java).findAll()
 
             val identifier: Int =
-                if (categoryRealmResults.max("id") != null) {
-                    categoryRealmResults.max("id")!!.toInt() + 1
+                if (categoryRealmResults2.max("id") != null) {
+                    categoryRealmResults2.max("id")!!.toInt()
                 } else {
                     0
                 }
@@ -63,10 +55,6 @@ class categorySet : AppCompatActivity() {
 
         realm.copyToRealmOrUpdate(mCategory)
         realm.commitTransaction()
-
-        categoryList.add(category)
-        Log.d("aaa",category)
-
 
         realm.close()
         finish()
